b8ac3ec214e249464327580d63039d0a2c3af6e4
aambernath <vivek@digambernath.org>
2025-01-25 11:45:41 +0400

    fixed tailedwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        albathaTeal: '#0aaabc',
+        albathaOrange: '#f18f37',
+        albathaDarkTeal: '#07848f',
+        albathaLightTeal: '#b2e2e4'
+      }
+    }
   },
-  plugins: [],
-}
-
+  plugins: []
+};
