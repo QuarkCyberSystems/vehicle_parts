da718d0146cbf9a7a0236f76e35139ce31e5a43e
aambernath <vivek@digambernath.org>
2025-01-25 10:57:49 +0400

    made copy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      // Proxy all calls starting with /api/v1 to your remote server
+      '/api/v1': {
+        target: 'https://parts.quarkcs.com',
+        changeOrigin: true
+        // If your server only supports HTTP, use 'http://', but typically you need https
+      }
+    }
+  }
 })
